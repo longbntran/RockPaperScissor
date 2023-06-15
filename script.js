5d4ecf148436c9a972d860ccdd3d7992a2db2946
longbntran <longbntran@gmail.com>
2023-06-15 13:07:41 +0700

    add scoreboard, buttons, announcement

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const RPS = ["ROCK", "PAPER", "SCISSOR"];
 function getComputerChoice() {
+    const RPS = ["ROCK", "PAPER", "SCISSOR"];
     return RPS[Math.floor(Math.random() * RPS.length)];
 }
 function playGame(userChoice,computerChoice) {
@@ -32,52 +32,56 @@ function playGame(userChoice,computerChoice) {
         }
     }
 }
-function play(e) {
-    const playerSelection = this.textContent;
-    const computerSelection = getComputerChoice();
-    const result = playGame(playerSelection, computerSelection);
-    let humanScore = parseInt(document.getElementById("human-score").textContent);
-    let computerScore = parseInt(document.getElementById("computer-score").textContent);
-    if (Math.max(humanScore, computerScore) === 5) {
-        let resultElement = document.getElementById("result");
-        resultElement.style.color = "blue";
-        let winner = humanScore > computerScore ? "The Hooman" : "The Mighty Comp";
-        resultElement.textContent = resultElement.textContent + "And the winner is: " + winner + `. Score: ${humanScore} - ${computerScore}`;
+function updateScore(result) {
+    switch(result) {
+        case "You Win!":
+            userScore += 1;
+            break;
+        case "You Lose!":
+            computerScore += 1;
+            break;
+        case "It's a Tie!":
+            break;
     }
-    console.log(humanScore,computerScore)
-    // const resultElement = document.getElementById("result");
-    // resultElement.textContent = result;
-    if (result === "You Win!") {
-        // resultElement.style.color = "green";
-        humanScore += 1;
-        document.getElementById("human-score").textContent = `${humanScore}`;
+    console.log(result,userScore, computerScore);
+    if (userScore<5 && computerScore<5) {
+        announcing.textContent = result;
+        tempUserScoreInHtml.textContent = userScore;
+        tempCompScoreInHtml.textContent = computerScore;
     }
-    else if (result === "You Lose!") {
-        // resultElement.style.color = "red";
-        computerScore += 1;
-        document.getElementById("computer-score").textContent = `${computerScore}`;
+    if (userScore===5) {
+        announcing.textContent = result;
+        tempUserScoreInHtml.textContent = userScore;
+        tempCompScoreInHtml.textContent = computerScore;
+        const alertMessage = document.getElementById("alert");
+        alertMessage.style.display = "block";
+        alertMessage.innerHTML = `
+        <span class="closebtn" onclick="window.location.reload()">&times;</span>
+        The winner is You ${userScore} against the Computer ${computerScore}.`;
     }
-    else if (result === "It's a Tie!") {
-        // resultElement.style.color = "black";
+    else if (computerScore===5) {
+        announcing.textContent = result;
+        tempUserScoreInHtml.textContent = userScore;
+        tempCompScoreInHtml.textContent = computerScore;
+        const alertMessage = document.getElementById("alert");
+        alertMessage.style.display = "block";
+        alertMessage.innerHTML = `
+        <span class="closebtn" onclick="document.location.reload()">&times;</span>
+        The winner is You ${userScore} against the Computer ${computerScore}.`
     }
-   
 }
-
-
-const buttons = document.querySelectorAll("button");
-buttons.forEach(button => button.addEventListener("click", play, {capture: false}));
-
-
-
-// Display the running score, and announce a winner of the game once one player reaches 5 points.
-// You will likely have to refactor (rework/rewrite) your original code to make it work for this. 
-// That’s OK! Reworking old code is an important part of a programmer’s life.
-
-
-// In our UI, the player should be able to play the game by clicking on buttons rather 
-// than typing their answer in a prompt.
-// For now, remove the logic that plays exactly five rounds.
-// Create three buttons, one for each selection. Add an event listener to the buttons that call your 
-// playRound function with the correct playerSelection every time a button is clicked. (you can keep
-//      the console.logs for this step)
-// Add a div for displaying results and change all of your console.logs into DOM methods.
+function checkWinner(userScore, computerScore) {
+    
+}
+const announcing = document.getElementById("announcing");
+const tempUserScoreInHtml = document.getElementById("user_score");
+const tempCompScoreInHtml = document.getElementById("computer_score");
+let userScore = 0;
+let computerScore = 0;
+const allButton = document.querySelectorAll("button");
+allButton.forEach(button => button.addEventListener("click", () => {
+    const userChoice = button.textContent;
+    const computerChoice = getComputerChoice();
+    const result = playGame(userChoice, computerChoice);
+    updateScore(result);
+}));
